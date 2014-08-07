70aa2cbaecadd9a6eb3f38b77b729c8d69034b3d
Luís Otávio Cobucci Oblonczyk <lcobucci@gmail.com>
2014-08-07 21:01:56 +0000

    Adding some comments so you can think about them

--- src/Notes/Model/Note.php
+++ src/Notes/Model/Note.php
@@ -1,84 +1,91 @@
 <?php
-
 namespace IW\NoteManager\Notes\Model;
+
+/* Havia faltado isto, a documentação do doctrine considera que as annotations
+ * estão sendo mágicamente registradas, quando na verdade devemos importá-las com o comando `use`.
+ *
+ * Vejam como estão sendo utilizadas as anotações no docblox da classe.
+ */
 
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity
  * @ORM\Table(name="note")
- **/
-class Note {
+ */
+class Note
+{
+    /**
+     * @var int
+     */
+    protected $id; // Cadê o mapeamento da coluna?
 
-	/**
-	 * @var int
-	 */
-	protected $id;
-	/**
-	 * @var string
-	 */
-	protected $title;
-	/**
-	 * @var string
-	 */
-	protected $content;
+    /**
+     * @var string
+     */
+    protected $title; // Cadê o mapeamento da coluna?
 
-	/**
-	 * @var string
-	 */
-	protected $slug;
+    /**
+     * @var string
+     */
+    protected $content; // Cadê o mapeamento da coluna?
 
-	/**
-	 * @var array
-	 */
-	protected $arrayNote = array();
+    /**
+     * @var string
+     */
+    protected $slug; // Cadê o mapeamento da coluna?
 
-	public function getId()
-	{
-		return $this->id;
-	}
+    /**
+     * @var array
+     */
+    protected $arrayNote = array(); // Pra que precisamos deste atributo?
 
-	public function getTitle()
-	{
-		return $this->title;
-	}
+    public function getId()
+    {
+        return $this->id;
+    }
 
-	public function setTitle($title)
-	{
-		$this->title = $title;
-		$this->arrayNote['title'] = $this->title;
-	}
+    public function getTitle()
+    {
+        return $this->title;
+    }
 
-	public function getContent()
-	{
-		return $this->content;
-	}
+    public function setTitle($title)
+    {
+        $this->title = $title;
+        $this->arrayNote['title'] = $this->title; // Pra que esta operação?
+    }
 
-	public function setContent($content)
-	{
-		$this->content = $content;
-		$this->arrayNote['content'] = $this->content;
-	}
+    public function getContent()
+    {
+        return $this->content;
+    }
 
-	public function getSlug()
-	{
-		return $this->slug;
-	}
+    public function setContent($content)
+    {
+        $this->content = $content;
+        $this->arrayNote['content'] = $this->content; // Pra que esta operação?
+    }
 
-	public function setSlug($slug)
-	{
-		$this->slug = $slug;
-		$this->arrayNote['slug'] = $this->slug;
-	}
+    public function getSlug()
+    {
+        return $this->slug;
+    }
 
-	public function createSlug($title)
-	{
-		return $title;
-	}
+    public function setSlug($slug) // Por que existe este método, ele a alteração do atributo poderia ser diretamente no setTile()?
+    {
+        $this->slug = $slug;
+        $this->arrayNote['slug'] = $this->slug;
+    }
 
-	public function toArray()
-	{
-		return $this->arrayNote;
-	}
-}
+    public function createSlug($title) // Por que este método é publico?
+    {
+        return $title;
+    }
+
+    public function toArray()
+    {
+        return $this->arrayNote;
+    }
+} // NUNCA utilize a tag de fechamento do PHP caso o arquivo tenha APENAS código PHP
 ?>
